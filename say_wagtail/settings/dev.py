3feec63c2d79c6b3788576165fa29214a39f88b7
AmirW <eng.amir.bu@gmail.com>
2022-08-18 11:42:04 +0430

    initial say_wagtail

---
+++ say_wagtail/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-ds%)=v@@@9p4z8(f5&b33k0%y@n!g8*x=9o=jaf)7s@$(kx6)q"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
