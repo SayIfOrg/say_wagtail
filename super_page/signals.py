ddb871c72972170b4e7ca4b211a7ed45ef54cf26
AmirW <eng.amir.bu@gmail.com>
2022-08-25 17:35:48 +0430

    simple html rendering for telegram

--- super_page/signals.py
+++ super_page/signals.py
@@ -1,10 +1,12 @@
 from django.dispatch import receiver
+from wagtail.blocks import RichTextBlock
 from wagtail.core.signals import page_published
+from wagtail.core.templatetags.wagtailcore_tags import richtext
+
+import grpc
 
 from say_wagtail.grpc import helloworld_pb2_grpc
 from say_wagtail.grpc import helloworld_pb2
-import grpc
-
 from .models import SimplePage
 
 
@@ -13,5 +15,11 @@ def send_grpc_on_save(instance, **kwargs):
     with grpc.insecure_channel("localhost:5060") as channel:
         stub = helloworld_pb2_grpc.GreeterStub(channel)
         # response = stub.SayHello(helloworld_pb2.HelloRequest(name="you"))
-        response = stub.PublishRichText(helloworld_pb2.RichText(body="\n".join([str(i.value) for i in instance.body])))
+        val = ""
+        for content in instance.body:
+            if isinstance(content.block, RichTextBlock):
+                val += str(richtext(content.value)) + "\n"
+            else:
+                val += content.value + "\n"
+        response = stub.PublishRichText(helloworld_pb2.RichText(body=val))
         print("Greeter client received: " + response.message)
