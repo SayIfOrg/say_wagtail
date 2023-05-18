7e81b7594b3179b7995f59759a18b2040f15301c
AmirW <eng.amir.bu@gmail.com>
2023-05-18 18:56:11 +0330

    Add ListablePage ability

--- say/page_types/models.py
+++ say/page_types/models.py
@@ -1,12 +1,34 @@
+from django import forms
+from django.utils.translation import gettext_lazy as _
 from wagtail import blocks
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import (
+    FieldPanel,
+    MultiFieldPanel,
+)
 from wagtail.fields import StreamField
 from wagtail.models import Page
 
-from grapple.models import GraphQLStreamfield
+from modelcluster.fields import ParentalManyToManyField
+from grapple.models import GraphQLStreamfield, GraphQLPage
 
 
-class SimplePage(Page):
+class ListablePage(Page):
+    max_count = 0
+
+    listings = ParentalManyToManyField(
+        "ListingPage",
+        related_name="listingpage_pages",
+        blank=True,
+        help_text=_("A reference to this page appear in witch ListablePages"),
+    )
+
+    graphql_fields = [
+        GraphQLPage("listingpage_pages", is_list=True),
+    ]
+
+
+class SimplePage(ListablePage):
+    max_count = None  # set to default
     parent_page_types = ["home.HomePage", "SimplePage"]
 
     body = StreamField(
@@ -19,6 +41,30 @@ class SimplePage(Page):
         FieldPanel("body"),
     ]
 
+    promote_panels = Page.promote_panels + [
+        MultiFieldPanel(
+            [FieldPanel("listings", widget=forms.CheckboxSelectMultiple)],
+            _("Include in ListablePages"),
+        ),
+    ]
+
     graphql_fields = [
+        GraphQLStreamfield("body"),
+        GraphQLPage("listings", is_list=True),
+    ]
+
+
+class ListingPage(Page):
+    body = StreamField(
+        [
+            ("editor", blocks.RichTextBlock()),
+        ],
+        use_json_field=True,
+    )
+    content_panels = Page.content_panels + [
+        FieldPanel("body"),
+    ]
+
+    graphql_fields = ListablePage.graphql_fields + [
         GraphQLStreamfield("body"),
     ]
